91992604eddee1abdca1fcabe8fa9297ce3568ff
Cameron Dix <cdix95@gmail.com>
2017-10-16 20:18:04 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,89 +1,63 @@
-#include <cassert>
 #include <iostream>
-#include <vector>
- 
+#include <cassert>
+
 using namespace std;
 
 template <class T>
-class Merge {
-public:
-    Merge(int a[], int s, int m, int e);
-    static void msort(int a[], int from, int to);
-    static bool sorted (int a[], int n);
-};
-
-// s = start 
-// m = middle
-// e = end 
-
-
-template <class T>
-Merge::Merge(int a[], int s, int m, int e) {
-    int n = e - s + 1;
-    vector<T> b(n);
-    int i1 = s;
-    int i2 = m + 1;
-    int j = 0;
-
-    while (i1 <= m && i2 <= e) {
-        if (a[i1] < a[i2]) {
-            b[j] = a[i1];
-            i1++;
-        }
-        else {
-            b[j] = a[i2];
-            i2++;
-        }
-        j++;
-    }
-
-    while (i1 <= m) {
-        b[j] = a[i1];
-        i1++;
-        j++;
-    }
-    while (i2 <= e) {
-        b[j] = a[i2];
-        i2++;
-        j++;
-    }
-    for (j = 0; j < n; j++)
-        a[s + j] = b[j];
+void msort(T a[], T x[], int s, int e) {
+	if(e - s > 1){
+		int m = (s+ (e - 1))/2;
+		msort(a, x, s, m+1);
+		msort(a, x, m+1, e);
+		int j = s;
+		int c = m+1;
+		int t = s;
+	    	while(j < m + 1 && c < e){
+			if(a[j] >= a[c]) x[t++] = a[c++];
+			else x[t++] = a[j++];
+	   	 }
+	   	 while(j < m+1) x[t++] = a[j++];
+	    	while(c < e) x[t++] = a[c++];
+	    	for(int q=s; q < e; q++) a[q] = x[q];
+	}
 }
 
 template <class T>
-void Merge::msort(int a[], int from, int to) {
-    if (from == to) return;
-    int m = (from + to) / 2;
-    msort(a, from, m);
-    msort(a, m + 1, to);
-    Merge(a, from, m, to);
+void mergesort(T a[], int n) {
+	T *x = new T[n];
+	msort(a, x, 0, n);
+	delete []x;
 }
 
 template <class T>
-bool Merge::sorted(int a[], int n) {
-    for (int i = 0; i < n -1; ++i) {
-        if (a[i] > a[i + 1])
-            return false;
-    }
-    return true;
+bool sorted(T a[], int n) {
+	for (int x = 0; x < n-1; x++)
+		if (a[x] > a[x+1]) return false;
+		return true;
 }
 
-int main (int argc, char * args[]) {
-    int a[1000];
-    for (int i = 0; i < 1000; ++i) a[i] = -50 + rand() % 100;
-    Merge::msort(a, 0, 999);
-    assert(Merge::sorted(a, 1000));
-    int b[1001];
-    for (int i = 0; i < 1001; ++i) b[i] = -50 + rand() % 100;
-    Merge::msort(b, 0, 1000);
-    assert(Merge::sorted(b, 1001));
-    int c[] = { 2 };
-    Merge::msort(c, 0, 1);
-    assert(Merge::sorted(c, 1));
-    int d[] = { 43, 7, -46, 57, 11 };
-    Merge::msort(d, 0, 4);
-    assert(Merge::sorted(d, 5));
-	
-    cout << "All tests passed." << endl;
+int main(int argc, char * args[]) {
+	int a[1000];
+	for (int i = 0; i < 1000; ++i) a[i] = -50 + rand() % 100;
+	mergesort(a, 1000);
+	assert(sorted(a, 1000));
+	int b[1001];
+	for (int i = 0; i < 1001; ++i) b[i] = -50 + rand() % 100;
+	mergesort(b, 1001);
+	assert(sorted(b, 1001));
+	int c[] = { 2 };
+	mergesort(c, 1);
+	assert(sorted(c, 1));
+	int d[] = { 1, 2, 3, 4, 5 };
+	mergesort(d, 5);
+	assert(sorted(d, 5));
+	int z [0];
+	mergesort (z, 0);
+	assert (sorted (z, 0));
+	double y []={5.8, 3.6, -6.9, 50.6, 4.7, -21.4, 6.12};
+	mergesort<double>(y, 7);
+	assert (sorted (y, 7));
+
+  
+	cout << "All tests passed." << endl;    
 }
